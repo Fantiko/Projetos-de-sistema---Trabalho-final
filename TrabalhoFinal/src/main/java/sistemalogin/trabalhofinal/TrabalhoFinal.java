2a59997bc53397d6f960b60e68101cd23227cecc
K.stefan <86378890+Fantiko@users.noreply.github.com>
2024-08-24 13:30:03 -0300

    criando o projeto no netbeans

---
+++ TrabalhoFinal/src/main/java/sistemalogin/trabalhofinal/TrabalhoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sistemalogin.trabalhofinal;
+
+/**
+ *
+ * @author kaios
+ */
+public class TrabalhoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
