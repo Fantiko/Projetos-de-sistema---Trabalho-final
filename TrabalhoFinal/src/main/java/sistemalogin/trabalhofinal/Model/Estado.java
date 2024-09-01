ac3d211cc10b37118210757492ab29ef68a3c05a
K.stefan <86378890+Fantiko@users.noreply.github.com>
2024-08-31 22:08:04 -0300

    acho que o model ta pronto

--- TrabalhoFinal/src/main/java/sistemalogin/trabalhofinal/Model/Estado.java
+++ TrabalhoFinal/src/main/java/sistemalogin/trabalhofinal/Model/Estado.java
@@ -12,6 +12,24 @@ public abstract class Estado {
         return this.getClass().getSimpleName().toLowerCase();
     }
 
+    public void criarUsuario(){
+        throw new RuntimeException("Não é possível criar usuario " + getNomeEstado());
+    }
+
+    public void aprovarUsuario(){
+        throw new RuntimeException("Não é possível aprovar usuario " + getNomeEstado());
+    }
+
+    public void deletarUsuario(){
+        throw new RuntimeException("Não é possível deletar usuario " + getNomeEstado());
+    }
+
+    public boolean isAprovado() {
+        throw new RuntimeException("Não é possível ver aprovacao " + getNomeEstado());
+    }
+    public void setAprovado() {
+        throw new RuntimeException("Não é possível setar aprocvacao " + getNomeEstado());
+    }
 
 
 }
